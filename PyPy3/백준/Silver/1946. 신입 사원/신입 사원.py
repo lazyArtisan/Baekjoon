a4c70b9973d16669832b550608c7497cbddec590
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-07-23 20:34:01 +0900

    [Silver I] Title: 신입 사원, Time: 4056 ms, Memory: 441292 KB -BaekjoonHub

--- PyPy3/백준/Silver/1946. 신입 사원/신입 사원.py
+++ PyPy3/백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,21 +1,30 @@
-# 1차 성적으로 sort 후에
-# 왼쪽부터 하나씩 순회를 한다
-# 지금까지의 2차 순위 중 최고와 비교하여 낮으면 탈락
-
 import sys
-input = sys.stdin.readline
-T = int(input())
+input = sys.stdin.read
+
+data = input().split()
+index = 0
+T = int(data[index])
+index += 1
+
+results = []
+
 for _ in range(T):
-    N = int(input())
+    N = int(data[index])
+    index += 1
     apply = []
     for _ in range(N):        
-        newbie = list(map(int,input().split()))
+        newbie = (int(data[index]), int(data[index + 1]))
         apply.append(newbie)
+        index += 2
+    
     apply.sort()
-    best_2nd = len(apply)+1
+    best_2nd = float('inf')
     cnt = 0
     for newbie in apply:   
         if newbie[1] < best_2nd:
             best_2nd = newbie[1]
             cnt += 1
-    print(cnt)
+    
+    results.append(cnt)
+
+print("\n".join(map(str, results)))
