7e82b605238bf7c51f9a5f1e37e12828ec251a8a
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-07-24 13:23:45 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- PyPy3/백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
+++ PyPy3/백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -1,40 +1,30 @@
-from collections import defaultdict, deque
-import sys
-input = sys.stdin.readline
+N, K = map(int,input().split())
+times = [[] for _ in range(K+1)]
+elec = list(map(int,input().split()))
+for i in range(K) :
+    times[elec[i]].append(i+1)
 
-N, K = map(int, input().split())
-L = list(map(int, input().split()))
-# 삭제 연산 비싸지 않은 큐를 사용
-order = defaultdict(deque)
-for i in range(len(L)):
-    order[L[i]].append(i)
-
+using = []
 cnt = 0
-box = set()
-# 다음에 가장 늦게 나올 물건을 먼저 뽑아버린다
-for j in range(max(N,K)):
-    # 현재 들어갈 물건의 현재 order 제거
-    order[L[j]].popleft()
-    if len(box) < N:
-        # 현재 들어가 있는 물건 수보다 박스 크기가 더 크다면
-        box.add(L[j])
-    else:
-        if L[j] in box:
-            # 현재 들어갈 물건이 박스에 이미 있다면 스킵
-            continue
-        else:
-            # 다음에 가장 늦게 나올 물건 고르기
-            latest_order, latest_obj = 0, 0
-            for obj in box:    
-                # 앞으로 안 나올거면 빼버려도 상관 없음
-                if not order[obj]:
-                    latest_obj = obj
-                    break        
-                elif latest_order < order[obj][0]:
-                    latest_order = order[obj][0]
-                    latest_obj = obj
-            box.remove(latest_obj)
-            box.add(L[j])
-            cnt += 1
-
+for i in elec :
+    if i in using :
+        times[i].pop(0)
+        continue
+    if len(using) < N :
+        using.append(i)
+        times[i].pop(0)
+    else :
+        target = 0
+        cnt += 1
+        for j in using :
+            if len(times[j]) == 0 :
+                using.remove(j)
+                break
+            target = max(times[j][0],target)
+            if target == times[j][0] :
+                del_item = j
+        if len(using) == N :
+            using.remove(del_item)
+        using.append(i)
+        times[i].pop(0)
 print(cnt)
