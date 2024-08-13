47b0f114a64ad5a43e43899ae8efdfec69fb18e0
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-08-13 12:51:25 +0900

    [Gold V] Title: 괄호의 값, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+brackets = list(input().strip())
+stack=[]
+
+# 유효한 괄호열인지 확인
+for brkt in brackets:
+    if stack:
+        if stack[-1] == '(' and brkt == ')':
+            stack.pop()
+            continue
+        elif stack[-1] == '[' and brkt == ']':
+            stack.pop()
+            continue
+    stack.append(brkt)
+
+if stack:
+    print(0)
+else:
+    for brkt in brackets:
+        if brkt == ')':
+            if stack[-1] == '(':
+                stack.pop()
+                stack.append(2)
+            else:
+                piece = 0
+                while(stack and stack[-1] != '('):
+                    piece += stack.pop()
+                stack.pop()
+                stack.append(2 * piece)
+        elif brkt == ']':
+            if stack[-1] == '[':
+                stack.pop()
+                stack.append(3)
+            else:
+                piece = 0
+                while(stack and stack[-1] != '['):
+                    piece += stack.pop()
+                stack.pop()
+                stack.append(3 * piece)
+        else:
+            stack.append(brkt)
+
+    print(sum(stack))
