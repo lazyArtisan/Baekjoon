2ef70298899469016f4d7591f4d215d306aa645f
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-08-19 00:35:19 +0900

    [Silver II] Title: 나무 자르기, Time: 424 ms, Memory: 258028 KB -BaekjoonHub

--- PyPy3/백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ PyPy3/백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,33 +1,21 @@
 import sys
-
-S = list(map(int, sys.stdin.readline().strip().split()))
-N = int(S[0]) # 나무의 수
-M = int(S[1]) # 필요한 나무의 길이
-T = list(map(int, sys.stdin.readline().strip().split())) # 나무들의 높이
-
-# h만큼 벌목기 올렸을 때 얻는 나무의 양
-def getTree(h):
-  sum = 0
-  for tree in T:
-    if tree - h > 0:
-      sum += tree - h
-  return sum
-
-start = 0
-end = max(T)
-mid = (start + end)//2
-
-while start <= end:
-  mid = (start+end)//2
-  Get = getTree(mid)
-
-  if Get == M:
-    break
-  elif Get <= M:
-    end = mid - 1
-    mid = (start+end)//2
-  else:
-    start = mid + 1
-    mid = (start+end)//2
-
-print(mid)
+input = sys.stdin.readline
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+pl, pr = 0, max(arr)
+while(pl <= pr):
+    res=0
+    p = (pl+pr)//2
+    for h in arr:
+        if h-p > 0:
+            res+=(h-p)
+    if M > res:
+        pr = p-1
+    elif M < res:
+        pl = p+1
+    else:
+        break
+if M > res:
+    print(p-1)
+else:
+    print(p)
