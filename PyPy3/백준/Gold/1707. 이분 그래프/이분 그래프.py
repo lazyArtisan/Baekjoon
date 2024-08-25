68f88715b39f9d678c9e0d39fd8d4d000ede080e
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-08-25 15:00:49 +0900

    [Gold IV] Title: 이분 그래프, Time: 576 ms, Memory: 186628 KB -BaekjoonHub

--- PyPy3/백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ PyPy3/백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,47 +1,31 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
-
-from collections import deque
-
-def bfs(node, visited, waiting):
-    visited.append(node)
-    waiting.append(node)
-    color[node] = 1
-    while len(waiting) != 0:
-        node = waiting.popleft()
-        for near in graph[node]:
-            if near not in visited:
-                waiting.append(near)
-                visited.append(near)
-            if color[near] is None:
-                color[near] = color[node] * -1
-            else:
-                if color[near] == color[node]:
-                    return False
-    return True
-
 K = int(input())
 for _ in range(K):
     V, E = map(int, input().split())
-    graph = [[] for _ in range(V+1)]
-    color = [None for _ in range(V+1)]
+    Vcolor = [0]*V
+    Vdict = {i : [] for i in range(V)}
+    # 간선 받아
     for _ in range(E):
-        a, b = map(int, input().split())
-        graph[a].append(b)
-        graph[b].append(a) 
-    
-    visited = []
-    waiting = deque()
-    bipartite = True
-
-    for i in range(1,V+1):
-        if i not in visited:
-            if not bfs(i,visited, waiting):
-                bipartite = False
-                break
-    if bipartite:
-        print('YES')
+        s, e = map(int, input().split())
+        Vdict[s-1].append(e-1)
+        Vdict[e-1].append(s-1)
+    # 간선 순회해
+    is2m = True
+    for i in range(V):
+        if Vcolor[i] == 0: # 방문 안 한 점이라면 dfs 시작
+            Vcolor[i] = 1
+            stack = [i] # dfs에 쓸 스택
+            while(stack and is2m):
+                j = stack.pop()
+                for k in Vdict[j]: # 딕셔너리에 있는 간선들 전부 방문
+                    if Vcolor[k]==0: # 방문 안 한 정점 찾았으면 스택에 해당 정점 추가하고 반대 색깔 색칠
+                        stack.append(k)
+                        Vcolor[k] = -Vcolor[j]
+                    elif Vcolor[k] == Vcolor[j]: # 이분 그래프가 아니었다면 종료
+                        is2m = False
+                        break
+    if is2m:
+        print("YES")
     else:
-        print('NO')
-            
+        print("NO")
