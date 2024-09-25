3500127e4eb6b82bb93cb257aaac3807b51371a0
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-09-25 14:35:07 +0900

    [Silver II] Title: 연속합, Time: 60 ms, Memory: 38828 KB -BaekjoonHub

--- Python/백준/Silver/1912. 연속합/연속합.py
+++ Python/백준/Silver/1912. 연속합/연속합.py
@@ -1,14 +1,7 @@
-n=int(input())
-seq=list(map(int,input().split()))
-Min = 0
-temp = 0
-Sum = 0
-res = -float('inf')
-for num in seq:
-    Sum+=num # 그래프 경로
-    temp+=num # 부분합
-    res = max(res, temp)
-    if Min > Sum: # 최소가 갱신되면 부분합 초기화 
-        Min = Sum
-        temp = 0
-print(res)
+n = int(input())
+a = list(map(int,input().split()))
+d = [0]
+for i in a:
+    if d[-1] > 0:d.append(i + d[-1])
+    else: d.append(i)
+print(max(d[1:]))
