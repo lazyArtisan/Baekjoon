c5c1f03927f3917f2234e3f6a40cccaf5bf35681
lazyArtisan <63232495+lazyArtisan@users.noreply.github.com>
2024-11-15 17:49:10 +0900

    [Silver V] Title: 숫자 카드, Time: 576 ms, Memory: 121536 KB -BaekjoonHub

--- Python/백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ Python/백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,18 +1,10 @@
-N=int(input())
-num_card=list(map(int,input().split()))
-M=int(input())
-check_list=list(map(int,input().split()))
-num_card.sort()
-for check_num in check_list:
-    find = 0
-    s,e=0,len(num_card)-1
-    while s<=e:
-        mid = (s+e)//2
-        if num_card[mid] > check_num:
-            e = mid-1
-        elif num_card[mid] < check_num:
-            s = mid+1
-        else:
-            find = 1
-            break
-    print(find, end=' ')
+N = int(input()) # 상근이가 갖고 있는 카드 수
+Nums = set(map(int,input().split())) # 숫자 카드에 적혀있는 정수
+M = int(input())
+Query = list(map(int,input().split())) # 숫자인지 아닌지 확인해야 하는 숫자들
+
+for q in Query:
+    if q in Nums:
+        print("1",end=" ")
+    else:
+        print("0",end=" ")
